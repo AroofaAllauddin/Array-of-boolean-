01378b4d576803de2434c98e361917d692151ce4
Aroofa Allauddin <160481304+AroofaAllauddin@users.noreply.github.com>
2024-03-09 00:57:19 +0500

    Add files via upload

---
+++ app.ts
@@ -0,0 +1,10 @@
+//Array of boolean//
+let booleanArray: boolean[] = [true, false, true, true, false];
+console.log(booleanArray); // Output: [true, false, true, true, false]
+
+
+
+//Array of numbers//
+let numberArray: number[] = [1, 2, 3, 4, 5];
+console.log(numberArray); // Output: [1, 2, 3, 4, 5]
+
